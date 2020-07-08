30da8043403b37f8f0abceb861ce356944eff06e
emWolf09 <maheshnagarwal1997@gmail.com>
2020-07-08 19:53:48 +0530

    adding screenshots

--- derbyUI/src/app/user/profile/profile.component.ts
+++ derbyUI/src/app/user/profile/profile.component.ts
@@ -9,9 +9,18 @@ export class ProfileComponent implements OnInit {
   followers:number=256;
   following:number  = 400;
   postCount:number = 21;
+  breakpoint:number = 2;
+  likeCount:number=400;
+  commentCount:number=34;
+  allPosts:number[] = [1,2,3,4,5,6];
+  imgUrl:any="https://material.angular.io/assets/img/examples/shiba2.jpg";
   constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.breakpoint = (window.innerWidth <= 700) ? 1 : 3;
   }
 
+  onResize(event) {
+    this.breakpoint = (event.target.innerWidth <= 700) ? 1 : 3;
+  }
 }
+
